7cc6286b36c73f569b854309eabe83eecabda64f
MX-Master <mikhail@vydrenko.ru>
2018-08-08 22:37:04 +0600

    Added blank stepgen header

---
+++ src/hal/drivers/allwinner_arisc/stepgen.h
@@ -0,0 +1,19 @@
+/**
+ * @file    stepgen.h
+ * @brief   STEPGEN related data
+ * @author  Mikhail Vydrenko (mikhail@vydrenko.ru)
+ */
+
+#ifndef _STEPGEN_H
+#define _STEPGEN_H
+
+#include "rtapi.h"
+#include "rtapi_app.h"
+#include "hal.h"
+
+#include "pulsgen.h"
+
+
+
+
+#endif
