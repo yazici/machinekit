86dc69f8572c099176abd7d31ac72e699a3e84e9
MX-Master <mikhail@vydrenko.ru>
2019-03-25 16:11:43 +0600

    addendum to previous commit

---
+++ src/hal/drivers/arisc/arisc.stepgen.h
@@ -0,0 +1,85 @@
+#ifndef _ARISC_STEPGEN_H
+#define _ARISC_STEPGEN_H
+
+#include "hal.h"
+
+
+
+
+#define STEPGEN_CH_CNT_MAX      16
+#define STEPGEN_DIR_TIME_MAX    200000 // max(dirhold + dirsetup), ns
+
+enum
+{
+    TASK_IDLE = 0,
+    TASK_FWD,
+    TASK_REV,
+    TASK_DIR_FWD,
+    TASK_DIR_REV,
+    TASK_FWD_DIR_REV,
+    TASK_REV_DIR_FWD
+};
+
+typedef struct
+{
+    // available for all functions
+
+    hal_bit_t *enable; // in
+    hal_u32_t *step_space; // in
+    hal_u32_t *step_len; // in
+    hal_u32_t *dir_setup; // in
+    hal_u32_t *dir_hold; // in
+    hal_u32_t *step_port; // in
+    hal_u32_t *step_pin; // in
+    hal_bit_t *step_inv; // in
+    hal_u32_t *dir_port; // in
+    hal_u32_t *dir_pin; // in
+    hal_bit_t *dir_inv; // in
+    hal_s32_t *counts; // out
+    hal_s32_t *rawcounts; // out
+
+    hal_s32_t counts_new; // private
+    hal_u32_t pulsgen_task; // private
+    hal_bit_t ctrl_type; // private
+
+    hal_u32_t step_space_old; // private
+    hal_u32_t step_len_old; // private
+    hal_u32_t step_port_old; // private
+    hal_u32_t step_pin_old; // private
+    hal_bit_t step_inv_old; // private
+    hal_u32_t step_pulsgen_ch0; // private
+    hal_u32_t step_pulsgen_ch1; // private
+    hal_bit_t step_ch_ready; // private
+    hal_s32_t step_freq_new; // private
+    hal_s32_t step_freq; // private
+    hal_s32_t step_wait_time; // private
+    hal_u32_t step_freq_max; // private
+    hal_u32_t step_accel_max; // private
+
+    hal_u32_t dir_port_old; // private
+    hal_u32_t dir_pin_old; // private
+    hal_bit_t dir_inv_old; // private
+    hal_u32_t dir_pulsgen_ch; // private
+    hal_bit_t dir_ch_ready; // private
+
+    // aren't available for the `make_pulses()`
+
+    hal_float_t *vel_max; // in
+    hal_float_t *accel_max; // in
+    hal_float_t *pos_scale; // in
+    hal_float_t *pos_cmd; // in
+    hal_float_t *vel_cmd; // in
+    hal_float_t *pos_fb; // out
+    hal_float_t *freq; // out
+
+    hal_float_t vel_max_old; // private
+    hal_float_t accel_max_old; // private
+    hal_float_t pos_cmd_old; // private
+    hal_float_t vel_cmd_old; // private
+
+} stepgen_ch_t;
+
+
+
+
+#endif
---
+++ src/hal/drivers/arisc/stepgen_api.h
@@ -0,0 +1,153 @@
+#ifndef _STEPGEN_API_H
+#define _STEPGEN_API_H
+
+#include <stdlib.h>
+#include "msg_api.h"
+
+
+
+
+enum
+{
+    STEPGEN_MSG_PIN_SETUP = 0x20,
+    STEPGEN_MSG_TASK_ADD,
+    STEPGEN_MSG_ABORT,
+    STEPGEN_MSG_POS_GET,
+    STEPGEN_MSG_POS_SET,
+    STEPGEN_MSG_TASKS_LEFT,
+    STEPGEN_MSG_CNT
+};
+
+
+
+
+/**
+ * @brief   setup GPIO pin for the selected channel
+ *
+ * @param   c               channel id
+ * @param   type            0:step, 1:dir
+ * @param   port            GPIO port number
+ * @param   pin             GPIO pin number
+ * @param   invert          invert pin state?
+ *
+ * @retval  none
+ */
+void stepgen_pin_setup(uint8_t c, uint8_t type, uint8_t port, uint8_t pin, uint8_t invert)
+{
+    u32_10_t *tx = (u32_10_t*) msg_buf;
+
+    tx->v[0] = c;
+    tx->v[1] = type;
+    tx->v[2] = port;
+    tx->v[3] = pin;
+    tx->v[4] = invert;
+
+    msg_send(STEPGEN_MSG_PIN_SETUP, msg_buf, 5*4, 0);
+}
+
+/**
+ * @brief   add a new task for the selected channel
+ *
+ * @param   c               channel id
+ * @param   type            0:step, 1:dir
+ * @param   pulses          number of pulses (ignored for DIR task)
+ * @param   pin_low_time    pin LOW state duration (in nanoseconds)
+ * @param   pin_high_time   pin HIGH state duration (in nanoseconds)
+ *
+ * @retval  none
+ */
+void stepgen_task_add(uint8_t c, uint8_t type, uint32_t pulses, uint32_t pin_low_time, uint32_t pin_high_time)
+{
+    u32_10_t *tx = (u32_10_t*) msg_buf;
+
+    tx->v[0] = c;
+    tx->v[1] = type;
+    tx->v[2] = pulses;
+    tx->v[3] = pin_low_time;
+    tx->v[4] = pin_high_time;
+
+    msg_send(STEPGEN_MSG_TASK_ADD, msg_buf, 5*4, 0);
+}
+
+/**
+ * @brief   abort all tasks for the selected channel
+ * @param   c   channel id
+ * @retval  none
+ */
+void stepgen_abort(uint8_t c)
+{
+    u32_10_t *tx = (u32_10_t*) msg_buf;
+
+    tx->v[0] = c;
+
+    msg_send(STEPGEN_MSG_ABORT, msg_buf, 1*4, 0);
+}
+
+/**
+ * @brief   set channel steps position
+ * @param   c   channel id
+ * @retval  integer 4-bytes
+ */
+int32_t stepgen_pos_get(uint8_t c)
+{
+    u32_10_t *tx = (u32_10_t*) msg_buf;
+
+    tx->v[0] = c;
+
+    msg_send(STEPGEN_MSG_POS_GET, msg_buf, 1*4, 0);
+
+    // finite loop, only 999999 tries to read an answer
+    uint32_t n = 0;
+    for ( n = 999999; n--; )
+    {
+        if ( msg_read(STEPGEN_MSG_POS_GET, msg_buf, 0) < 0 ) continue;
+        else return (int32_t)tx->v[0];
+    }
+
+    return 0;
+}
+
+/**
+ * @brief   set channel steps position
+ * @param   c       channel id
+ * @param   pos     integer 4-bytes
+ * @retval  none
+ */
+void stepgen_pos_set(uint8_t c, int32_t pos)
+{
+    u32_10_t *tx = (u32_10_t*) msg_buf;
+
+    tx->v[0] = c;
+    tx->v[1] = (uint32_t)pos;
+
+    msg_send(STEPGEN_MSG_POS_SET, msg_buf, 2*4, 0);
+}
+
+/**
+ * @brief   task count left to do
+ * @param   c           channel id
+ * @retval  uint8_t     task count left to do (including current task)
+ */
+uint8_t stepgen_tasks_left(uint8_t c)
+{
+    u32_10_t *tx = (u32_10_t*) msg_buf;
+
+    tx->v[0] = c;
+
+    msg_send(STEPGEN_MSG_TASKS_LEFT, msg_buf, 1*4, 0);
+
+    // finite loop, only 999999 tries to read an answer
+    uint32_t n = 0;
+    for ( n = 999999; n--; )
+    {
+        if ( msg_read(STEPGEN_MSG_TASKS_LEFT, msg_buf, 0) < 0 ) continue;
+        else return (uint8_t)tx->v[0];
+    }
+
+    return 0;
+}
+
+
+
+
+#endif
